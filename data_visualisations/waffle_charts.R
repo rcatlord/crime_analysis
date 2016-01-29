f6db67a2844aac8b3f0b3ea2974fdfb041014719
Henry Partridge <hcpartridge@HENRYs-MacBook-Pro.local>
2016-01-29 13:18:43 +0000

    Initial commit

---
+++ data_visualisations/waffle_charts.R
@@ -0,0 +1,30 @@
+## Waffle charts ##
+
+# Load the necessary packages
+library(dplyr) ; library(tidyr)
+
+# Set your working directory to where the crime data are stored
+setwd("../")
+
+# Read the data
+crimes <- read.csv("crime_data.csv", header = T)
+
+# Frequency of crime by borough (in descending order)
+df <- crimes %>% 
+  filter(month == "2015-11-01" & category == "Robbery") %>% 
+  count(borough, sort = TRUE) %>%
+  mutate(percent = round(n/sum(n)*100, 1)) %>% 
+  select(borough, percent) %>% 
+  spread(borough, percent)
+
+df <- df[, order(-df[which(rownames(df) == '1'), ]) ]
+
+# Use waffle
+library(waffle) 
+waffle(df, rows = 4, size = 2, 
+       colors=(RColorBrewer::brewer.pal(n=10,"Set3")),
+       title="Robbery as a proportion of total Robbery offences, 11/2015",
+       legend_pos = "bottom")
+ggsave("waffle.png", scale=1.5, dpi=300)
+
+
