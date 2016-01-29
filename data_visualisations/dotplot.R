e366953bdaa62055fe5935a4c12cd8ade6190e4e
Henry Partridge <pracademic.io@gmail.com>
2016-01-29 09:38:08 +0000

    Update dotplot.R

--- data_visualisations/dotplot.R
+++ data_visualisations/dotplot.R
@@ -20,8 +20,9 @@ df <- crimes %>%
   ungroup() %>%
   arrange(desc(n))
 
-# Create a Cleveland dotplot using ggplot2
+# Create a dotplot using ggplot2
 library(ggplot2)
+
 ggplot(df, aes(x = reorder(borough, n), y = n)) + 
   geom_point(size = 12, stat = "identity", color = "black") + 
   geom_text(aes(label = n, fontface = "bold"), color = "white", size = 4) + 
@@ -33,3 +34,22 @@ ggplot(df, aes(x = reorder(borough, n), y = n)) +
 
 # Save the dotplot as a png file
 ggsave("dotplot.png", scale = 1.5, dpi = 300)
+
+# Create an interactive dotplot using rcdimple
+library(rcdimple)
+
+crimes %>% 
+  filter(month == "2015-11-01", 
+         category == "Bicycle theft" | category == "Burglary" | category == "Vehicle crime") %>% 
+  group_by(category, borough) %>%
+  summarise(n = n()) %>% 
+  dimple(x = "n", y = "borough", groups = "category", type = "bubble", height = 500, width = 1000) %>%
+    xAxis(type = "addMeasureAxis", showGridlines = F, title = "Number of crimes") %>%
+    yAxis(type = "addCategoryAxis", orderRule = "Frequency", showGridlines = F, title = "Borough") %>%
+    add_title(html = paste0("<div style='text-align:center;width:100%'>
+                                <b style = 'font-size:100%;'>", paste('Crimes in Greater Manchester during November 2015'),
+                            "</div>")) %>% 
+    add_legend(x = 500, y = 20, # position the legend on the chart
+               # width = 500,
+               # height = 20, 
+               horizontalAlign = "right")
