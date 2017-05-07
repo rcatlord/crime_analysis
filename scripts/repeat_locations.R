2e93e4ccf90542fec2453eae09fc6f91c0b49f06
Henry Partridge <h.partridge@mmu.ac.uk>
2017-05-07 08:58:21 +0100

    Update repeat_locations.R

--- scripts/repeat_locations.R
+++ scripts/repeat_locations.R
@@ -1,7 +1,7 @@
 ## Repeat locations ##
 
 # Load the necessary packages
-library(dplyr)
+library(tidyverse) ; library(leaflet)
 
 # Set your working directory to where the crime data are stored
 setwd("../")
@@ -27,14 +27,13 @@ rpt_locs <- crimes %>%
   slice(1:10)
 
 popup <- paste0("<strong>Frequency: </strong>", rpt_locs$n,
-  "<br><strong>Location: </strong>", rpt_locs$location,
-  "<br><strong>Borough: </strong>", rpt_locs$borough)
+                "<br><strong>Location: </strong>", rpt_locs$location,
+                "<br><strong>Borough: </strong>", rpt_locs$borough)
 
-library(leaflet)
 leaflet() %>%
   addProviderTiles("CartoDB.Positron") %>% 
   addCircleMarkers(data = rpt_locs, 
-    ~long, ~lat, 
-    fillColor = "white", color = "red",  
-    radius = ~n, # this may need to be controlled e.g. radius = ~n*0.1
-    popup = ~popup) 
+                   ~long, ~lat, 
+                   fillColor = "white", color = "red",  
+                   radius = ~n, # this may need to be controlled e.g. radius = ~n*0.1
+                   popup = ~popup) 
